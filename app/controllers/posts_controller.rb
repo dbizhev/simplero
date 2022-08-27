bc1013921241f4d996fd68e58d9798c331b207da
josh-gehmlich <joshuagehmlich29@gmail.com>
2022-08-26 23:35:32 -0400

    add edit and delete post functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :active_link
-  before_action :set_group, only: [:index, :create]
+  before_action :set_group, only: [:index, :create, :edit, :update, :destroy]
+  before_action :set_post, only: [:edit, :update, :destroy]
 
   def index
     @members = @group.members.includes(:user).where(status: Members::Status::ACCEPTED)
@@ -25,6 +26,33 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    render turbo_stream: turbo_stream.update("group_#{@group.id}_post_#{@post.id}", render_to_string(Posts::FormComponent.new(post: @post)))
+  end
+
+  def update
+    if @post.update(post_params)
+      Turbo::StreamsChannel.broadcast_update_to "group_#{@group.id}_posts_stream", html: render_to_string(Posts::ListItemComponent.new(group: @group, post: @post)), target: "group_#{@group.id}_post_#{@post.id}"
+
+      render turbo_stream: turbo_stream.update('turbo_messages', render_to_string(Shared::TurboMessageComponent.new(notice: "Post successfully updated")))
+    else
+      render turbo_stream: [
+        turbo_stream.update('turbo_messages', render_to_string(Shared::TurboMessageComponent.new(alert: "Oops, something went wrong"))),
+        turbo_stream.update("group_#{@group.id}_post_#{@post.id}", render_to_string(Posts::FormComponent.new(post: @post)))
+      ]
+    end
+  end
+
+  def destroy
+    if @post.destroy
+      Turbo::StreamsChannel.broadcast_update_to "group_#{@group.id}_posts_stream", target: "group_#{@group.id}_post_#{@post.id}"
+
+      render turbo_stream: turbo_stream.update('turbo_messages', render_to_string(Shared::TurboMessageComponent.new(notice: "Post successfully deleted")))
+    else
+      render turbo_stream: turbo_stream.update('turbo_messages', render_to_string(Shared::TurboMessageComponent.new(alert: "Oops, something went wrong")))
+    end
+  end
+
   private
 
   def active_link
@@ -33,6 +61,10 @@ class PostsController < ApplicationController
 
   def set_group
     @group = Group.find(params[:group_id])
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
   def post_params
