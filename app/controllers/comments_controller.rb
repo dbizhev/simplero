14f7fe9ff4db06415f8e7a6863694b07c725ed22
josh-gehmlich <joshuagehmlich29@gmail.com>
2022-08-29 16:46:21 -0400

    update and destroy comment functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,8 @@
 class CommentsController < ApplicationController
   before_action :active_link
-  before_action :set_group, only: [:index, :create]
-  before_action :set_post, only: [:index, :create]
+  before_action :set_group
+  before_action :set_post
+  before_action :set_comment, only: [:edit, :update, :destroy]
 
   def index
     @comments = @post.comments.includes(post: :group)
@@ -24,6 +25,33 @@ class CommentsController < ApplicationController
 
   end
 
+  def edit
+    render turbo_stream: turbo_stream.update("group_#{@group.id}_post_#{@post.id}_comment_#{@comment.id}", render_to_string(Comments::FormComponent.new(comment: @comment)))
+  end
+
+  def update
+    if @comment.update(comment_params)
+      Turbo::StreamsChannel.broadcast_update_to "group_#{@group.id}_post_#{@post.id}_comments_stream", html: render_to_string(Comments::ListItemComponent.new(comment: @comment)), target: "group_#{@group.id}_post_#{@post.id}_comment_#{@comment.id}"
+
+      render turbo_stream: turbo_stream.update('turbo_messages', render_to_string(Shared::TurboMessageComponent.new(notice: "Comment successfully updated")))
+    else
+      render turbo_stream: [
+        turbo_stream.update('turbo_messages', render_to_string(Shared::TurboMessageComponent.new(alert: "Oops, something went wrong"))),
+        turbo_stream.update("group_#{@group.id}_post_#{@post.id}_comment_#{@comment.id}", render_to_string(Comments::FormComponent.new(comment: @comment)))
+      ]
+    end
+  end
+
+  def destroy
+    if @comment.destroy
+      Turbo::StreamsChannel.broadcast_update_to "group_#{@group.id}_post_#{@post.id}_comments_stream", target: "group_#{@group.id}_post_#{@post.id}_comment_#{@comment.id}"
+
+      render turbo_stream: turbo_stream.update('turbo_messages', render_to_string(Shared::TurboMessageComponent.new(notice: "Comment successfully deleted")))
+    else
+      render turbo_stream: turbo_stream.update('turbo_messages', render_to_string(Shared::TurboMessageComponent.new(alert: "Oops, something went wrong")))
+    end
+  end
+
   private
 
   def active_link
@@ -36,6 +64,10 @@ class CommentsController < ApplicationController
 
   def set_post
     @post = Post.find(params[:post_id])
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
   end
 
   def comment_params
